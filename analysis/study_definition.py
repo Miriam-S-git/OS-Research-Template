a48d545b9305f6d50b5b67cb32748d04058f4ad8
Miriam-S-git <74236842+Miriam-S-git@users.noreply.github.com>
2022-01-26 16:50:21 +0000

    Update study_definition.py

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,55 +1,64 @@
 ###  Study definition for BMI
 
+
+
+#####################
+# Import statements #
+#####################
+
 from cohortextractor import (
     StudyDefinition,
-    codelist,
-    codelist_from_csv,
-    combine_codelists,
-    filter_codes_by_category,
     patients,
+    codelist_from_csv,
+    codelist,
+    filter_codes_by_category,
+    combine_codelists,
+    Measure,
 )
 
-################################
+from common_variables import (
+    common_variables
+)
+
+from codelists import *
+import pyarrow.feather as feather
+
+######################
+#  Study definition  #
+######################
+
+# Define start and end dates
+
 study = StudyDefinition(
-    # define default dummy data behaviour
+    
+    # Set time period
     default_expectations={
-        "date": {"earliest": "1970-01-01", "latest": "today"},
+        "date": {"earliest": "1900-01-01", "latest": "today"},
         "rate": "uniform",
         "incidence": 0.2,
     },
-
-    # define the study index date
-    index_date="2020-03-01",
-
-    # define the study population
+    
+    # Limiting to patients who had an HbA1c test 
     population=patients.satisfying(
         """
-        registered AND
-        (sex = 'M' OR sex = 'F') AND  ##  Do i need to define sex somewhere?
+        registered_one AND
+        (sex = 'M' OR sex = 'F') AND
         (age >= 23 AND age <= 110) AND
+        (diabetes_type != 'UNKNOWN_DM') AND
         (region != '')
         """,
         # Indicator for registration
-        registered = patients.registered_with_one_practice_between(
-    "2019-03-01", "2020-02-01"
-    ),
-
-    # define the study variables
-    age=patients.age_as_of("index_date"),
-        
-    sex=patients.sex(
-        return_expectations={
-            "category": {"ratios": {"M": 0.49, "F": 0.51}},
-            "incidence": 1,
-        }
-    ),
-    # more variables ...
+        registered_one=patients.registered_with_one_practice_between(
+            start_date="2019-03-01",
+            end_date="2020-03-01",
+            return_expectations={"incidence": 0.90},
+        ),            
+    **common_variables
 )
 
-
-
-
-
+######################
+#      Measures      #
+######################
 
 
 
