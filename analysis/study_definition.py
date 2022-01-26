bb316262c5a28a54c72bf20cbc342956d59372f6
Miriam-S-git <74236842+Miriam-S-git@users.noreply.github.com>
2022-01-26 16:19:46 +0000

    Update study_definition.py

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -9,22 +9,47 @@ from cohortextractor import (
     patients,
 )
 
-
+################################
 study = StudyDefinition(
+    # define default dummy data behaviour
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": "1970-01-01", "latest": "today"},
         "rate": "uniform",
-        "incidence": 0.5,
+        "incidence": 0.2,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+
+    # define the study index date
+    index_date="2020-03-01",
+
+    # define the study population
+    population=patients.satisfying(
+        """
+        registered AND
+        (sex = 'M' OR sex = 'F') AND  ##  Do i need to define sex somewhere?
+        (age >= 23 AND age <= 110) AND
+        (region != '')
+        """,
+        # Indicator for registration
+        registered = patients.registered_with_one_practice_between(
+    "2019-03-01", "2020-02-01"
     ),
 
-    age=patients.age_as_of(
-        "2019-09-01",
+    # define the study variables
+    age=patients.age_as_of("index_date"),
+        
+    sex=patients.sex(
         return_expectations={
-            "rate": "universal",
-            "int": {"distribution": "population_ages"},
-        },
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+            "incidence": 1,
+        }
     ),
+    # more variables ...
 )
+
+
+
+
+
+
+
+
