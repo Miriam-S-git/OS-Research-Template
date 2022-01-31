e7e7daa35262f98f687ebe83f1f8d91379ce91fb
Miriam-S-git <miriam.samuel@nhs.net>
2022-01-31 12:07:27 +0000

    how to add codelists

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -93,14 +93,7 @@ common_variables = dict(
                 }
             },
         },
-    ),   
-    # Dementia
-    dementia=patients.with_these_clinical_events(
-        dementia-complete_codes,
-        on_or_before="index_date",
-        returning="binary_flag",
-        return_expectations={"incidence": 0.02, },
-    ),       
+    ),         
 )
     
 
