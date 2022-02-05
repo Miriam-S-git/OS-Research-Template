e7754b4f41026b61e23889cffcd1ccc5a06d6b1f
Miriam-S-git <74236842+Miriam-S-git@users.noreply.github.com>
2022-02-05 17:10:39 +0000

    Update study_definition.py

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -16,10 +16,37 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
+    
+    ############################### Robin's code
+    population=patients.satisfying(
+        """
+        registered AND
+        (NOT died) AND
+        (sex = "M" OR sex = "F") AND
+        (age >= 18 AND age <= 110) AND
+        (region != "")
+        """,
+        registered=patients.registered_as_of(
+            "index_date",
+            return_expectations={"incidence": 0.9},
+        ),
+        died=patients.died_from_any_cause(
+            on_or_before=end_date,
+            returning="binary_flag",
+            return_expectations={"incidence": 0.1}
+        )
+    ),
+    
+    
+    
+    
+   ##################################### 
+   ##################################### MY old code - changed to just define a variable
+    registered=patients.registered_with_one_practice_between(
         "2019-03-01", "2020-03-01"
     ),
     
+    ##############################################
     ** common_variables
 )
 from codelists import *
